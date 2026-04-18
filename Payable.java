cd1926b7704eec0f9f92ebd13a2464a1b827c7c9
poulaplus5-art <poulaplus5@gmail.com>
2026-04-18 10:44:30 +0200

    Update from VR Code City

--- Payable.java
+++
@@ -1,6 +0,0 @@
-// Interface 3: anything that involves payment
-public interface Payable {
-    boolean processPayment(double amount);
-    double getOutstandingBalance();
-    void printReceipt();
-}
